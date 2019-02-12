9019fe54959c49345a62e39d08d7dd807c714434
MartinVoronak <voronak.martin@gmail.com>
2019-02-12 17:42:26 +0100

    finished Retrieve matches API

--- app/src/main/java/com/example/martin/krive_hokejky/APIcalls.java
+++ app/src/main/java/com/example/martin/krive_hokejky/APIcalls.java
@@ -4,27 +4,31 @@ import com.airbnb.lottie.LottieAnimationView;
 import com.backendless.Backendless;
 import com.backendless.async.callback.AsyncCallback;
 import com.backendless.exceptions.BackendlessFault;
+import com.backendless.persistence.DataQueryBuilder;
 import com.example.martin.krive_hokejky.Activities.AddMatchActivity;
 import com.example.martin.krive_hokejky.Activities.AddPlayerActivity;
 import com.example.martin.krive_hokejky.DataObjects.Match;
 import com.example.martin.krive_hokejky.DataObjects.Player;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 import static com.example.martin.krive_hokejky.Constants.LOG_BACKANDLESS;
 
 public enum APIcalls implements Serializable {
 
-    CREATE_PLAYER, CREATE_MATCH, RETRIEVE_PLAYERS;
-    public static boolean inAPIcall = false;
+    CREATE_PLAYER, CREATE_MATCH, RETRIEVE_PLAYERS, RETRIEVE_FUTURE_MATCHES;
 
-    public static void changeStateAPI(){
+    public static boolean inAPIcall = false;
+    public static List<Player> players;
+    public static List<Match> matches;
+
+    public static void changeStateAPI() {
         inAPIcall = !inAPIcall;
     }
-
-    public static List<Player> players;
-
 
     public static void doAction(APIcalls action, final LottieAnimationView animationView) {
         switch (action) {
@@ -46,7 +50,7 @@ public enum APIcalls implements Serializable {
 
                             @Override
                             public void handleResponse(Player response) {
-                                responseOK(animationView, ""+response);
+                                responseOK(animationView, "" + response);
                             }
 
                             // an error has occurred, the error code can be retrieved with fault.getCode()
@@ -70,7 +74,7 @@ public enum APIcalls implements Serializable {
                     public void handleResponse(List<Player> foundContacts) {
                         players = foundContacts;
 
-                        Utilities.log(LOG_BACKANDLESS, "response: classes loaded");
+                        Utilities.log(LOG_BACKANDLESS, "response: players loaded");
                         changeStateAPI();
                         animationView.cancelAnimation();
                     }
@@ -91,12 +95,12 @@ public enum APIcalls implements Serializable {
 
                 //fetch matches from activity list
                 List<Match> matchesToSend = AddMatchActivity.futureMatches;
-                if (!matchesToSend.isEmpty()){
+                if (!matchesToSend.isEmpty()) {
 
-                    Backendless.Data.of( Match.class ).create( matchesToSend, new AsyncCallback<List<String>>() {
+                    Backendless.Data.of(Match.class).create(matchesToSend, new AsyncCallback<List<String>>() {
                         @Override
                         public void handleResponse(List<String> response) {
-                            responseOK(animationView, ""+response);
+                            responseOK(animationView, "" + response);
                         }
 
                         @Override
@@ -107,13 +111,62 @@ public enum APIcalls implements Serializable {
                 }
                 break;
 
+            case RETRIEVE_FUTURE_MATCHES:
+
+                changeStateAPI();
+                Utilities.log(Constants.LOG_BACKANDLESS, "API call Retrieve matches");
+                animationView.loop(true);
+                animationView.playAnimation();
+
+                Backendless.Data.of(Player.class).find(new AsyncCallback<List<Player>>() {
+                    @Override
+                    public void handleResponse(List<Player> foundContacts) {
+                        players = foundContacts;
+                        Utilities.log(LOG_BACKANDLESS, "response: players loaded");
+                    }
+
+                    @Override
+                    public void handleFault(BackendlessFault fault) {
+                        responseNotOK(animationView, fault.getMessage());
+                        return;
+                    }
+                });
+
+                Date now = new Date();
+                String myFormat = "MM.dd.yyyy HH:mm:ss";
+                SimpleDateFormat dateFormat = new SimpleDateFormat(myFormat, Locale.GERMANY);
+                String dateToCompare = dateFormat.format(now);
+
+                String whereClause = "dateMatch < '" + dateToCompare + "'";
+                DataQueryBuilder queryBuilder = DataQueryBuilder.create();
+                queryBuilder.setWhereClause(whereClause);
+
+                Backendless.Data.of(Match.class).find(queryBuilder,
+                        new AsyncCallback<List<Match>>() {
+                            @Override
+                            public void handleResponse(List<Match> foundMatches) {
+                                matches = foundMatches;
+
+                                Utilities.log(LOG_BACKANDLESS, "response: matches loaded");
+                                changeStateAPI();
+                                animationView.cancelAnimation();
+                            }
+
+                            @Override
+                            public void handleFault(BackendlessFault fault) {
+                                responseNotOK(animationView, fault.getMessage());
+                            }
+                        });
+
+                break;
+
             default:
                 System.out.println("Midweek days are so-so.");
                 break;
         }
     }
 
-    public static void responseOK(LottieAnimationView animationView, String response){
+    public static void responseOK(LottieAnimationView animationView, String response) {
         animationView.cancelAnimation();
 
         Utilities.setAnimationName(animationView, "success.json");
@@ -123,7 +176,7 @@ public enum APIcalls implements Serializable {
         Utilities.log(LOG_BACKANDLESS, "response: " + response);
     }
 
-    public static void responseNotOK(LottieAnimationView animationView, String response){
+    public static void responseNotOK(LottieAnimationView animationView, String response) {
         animationView.cancelAnimation();
 
         Utilities.setAnimationName(animationView, "error.json");
